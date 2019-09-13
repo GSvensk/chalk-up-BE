a50043c6f5fa06e3c3207326abf291fdc07a604f
Gustav Svensk <gustav.svensk95@gmail.com>
2019-09-13 16:05:22 +0200

    add sessiontests and supertest dependency

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const config = require('./utils/config')
+const sessionsRouter = require('./controllers/sessions')
 
-
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
-console.log('connecting to', config.MONGODB_URI)
+try {
+    mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+    console.log('connecting to', config.MONGODB_URI)
+} catch (error) {
+    console.log('mongoose connection failed')
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
+
+app.use('/api/sessions', sessionsRouter)
 
 module.exports = app
---
+++ tests/sessions/get_sessions.test.js
@@ -0,0 +1,66 @@
+const supertest = require('supertest')
+const app = require('../../app')
+const Session = require('../../models/session')
+const User = require('../../models/user')
+const mongoose = require('mongoose')
+const api = supertest(app)
+
+
+const initialSessions = [
+        {
+            dateTime: new Date(),
+            location: "hangaren",
+            creator: "5d7b94e9d7a1dc14cd5aaffc",
+            participants: [],
+            info: ""
+        },
+        {
+            dateTime: new Date(),
+            location: "akalla",
+            creator: "5d7b94e9d7a1dc14cd5aaffc",
+            participants: [],
+            info: ""
+        }
+]
+
+beforeEach(async () => {
+    await Session.deleteMany({})
+
+    const sessionObjects = initialSessions
+        .map(session => new Session(session))
+    const promises = sessionObjects.map(session => session.save())
+    await Promise.all(promises)
+
+
+    await User.deleteMany({})
+    const user = new User({ username: 'session_test', name: 'test sesson' })
+    await user.save()
+})
+
+describe('when there is initially some sessions saved', () => {
+    test('sessions are returned as json', async () => {
+        await api
+            .get('/api/sessions')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+
+    test('the correct amount of sessions is returned', async () => {
+        const res = await api.get('/api/sessions')
+        expect(res.body.length).toBe(initialSessions.length)
+    })
+
+    test('the unique identifier property of the session posts is named id', async () => {
+        const res = await api.get('/api/sessions')
+        expect(res.body[0].id).toBeDefined()
+    })
+
+    test('the unique identifier property of the session posts is not named _id', async () => {
+        const res = await api.get('/api/sessions')
+        expect(res.body[0]._id).not.toBeDefined()
+    })
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
