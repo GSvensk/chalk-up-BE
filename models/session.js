7857e4e739eb903d20e74889bf9563c00205314a
Gustav Svensk <gustav.svensk95@gmail.com>
2019-09-13 15:17:13 +0200

    add session model

---
+++ models/session.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const sessionSchema = mongoose.Schema({
+  dateTime: {
+    type: Date,
+    required: true
+  },
+  location: String,
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  participants: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  info: String
+})
+
+sessionSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Session', sessionSchema)
