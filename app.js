4575fae1cd3b6c0e18bc9d361dde52e48b30374e
Gustav Svensk <gustav.svensk95@gmail.com>
2019-09-13 12:44:43 +0200

    add moongoose dep

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+const mongoose = require('moongoose')
+const config = require('./utils/config')
+
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+console.log('connecting to', config.MONGODB_URI)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
