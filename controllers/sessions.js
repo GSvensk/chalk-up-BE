b2b931b15e80bc1d21083d3d863d8e2ef8474f50
Gustav Svensk <gustav.svensk95@gmail.com>
2019-09-13 16:28:37 +0200

    add sessionsRouter POST

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,12 +1,32 @@
 const sessionsRouter = require('express').Router()
 const Session = require('./../models/session')
 
-sessionsRouter.get('/', async (request, response) => {
+sessionsRouter.get('/', async (request, response, next) => {
     try {
         const sessions = await Session
             .find({})
 
         response.json(sessions.map(session => session.toJSON()))
+    } catch (error) {
+        next(error)
+    }
+})
+
+sessionsRouter.post('/', async (request, response, next) => {
+
+    const body = request.body
+
+    try {
+        const session = new Session({
+            dateTime: body.dateTime,
+            location: body.location,
+            creator: body.creatorId,
+            participants: body.participants,
+            info: body.info
+        })
+
+        const savedSession = await session.save()
+        response.json(savedSession.toJSON())
     } catch (error) {
         next(error)
     }
