ea807b3db8ed3fd0b1fbc07aa99539d245d22bb8
Gustav Svensk <gustav.svensk95@gmail.com>
2019-09-13 15:18:07 +0200

    add test for adding session to test db

--- tests/dummy_db.test.js
+++ tests/dummy_db.test.js
@@ -1,21 +1,52 @@
 const User = require('../models/user')
+const Session = require('../models/session')
 const helper = require('./test_helper')
 const mongoose = require('mongoose')
 const app = require('../app')
 
+describe('when there is initially one user at db', () => {
 
-test('creation succeeds with a fresh user', async () => {
-    await User.deleteMany({})
-    const user = new User({ username: 'test', name: 'test rootson' })
-    await user.save()
+    beforeEach(async () => {
+        await User.deleteMany({})
+        const user = new User({ username: 'test', name: 'test rootson' })
+        await user.save()
+    })
 
-    const usersAtEnd = await helper.usersInDb()
-    expect(usersAtEnd.length).toBe(1)
+    test('creation succeeds with a fresh user', async () => {
+
+        const user = new User({ username: 'testaren', name: 'steven' })
+        await user.save()
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd.length).toBe(2)
+
+        const usernames = usersAtEnd.map(u => u.username)
+        expect(usernames).toContain(user.username)
+    })
+
     
-    const usernames = usersAtEnd.map(u => u.username)
-    expect(usernames).toContain(user.username)
+        test('sessioncreation works', async () => {
+            await Session.deleteMany({})
+            const session = new Session(
+                {
+                    dateTime: new Date(),
+                    location: "hangaren",
+                    creator: "5d7b94e9d7a1dc14cd5aaffc",
+                    participants: [],
+                    info: ""
+                })
+            await session.save()
+    
+            const sessionsAtEnd = await helper.sessionsInDb()
+            expect(sessionsAtEnd.length).toBe(1)
+    
+            const sessionDates = sessionsAtEnd.map(sess => sess.dateTime)
+            expect(sessionDates).toContainEqual(session.dateTime)
+        })
+     
+    
+    afterAll(() => {
+        mongoose.connection.close()
+    })
 })
 
-afterAll(() => {
-    mongoose.connection.close()
-})
