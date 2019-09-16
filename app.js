a70be2856792fe7dfe3ac02a744c12d66d88198a
Gustav Svensk <gustav.svensk95@gmail.com>
2019-09-16 10:35:04 +0200

    add GET user route

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const sessionsRouter = require('./controllers/sessions')
 const usersRouter = require('./controllers/users')
+const userRouter = require('./controllers/user')
+
 
 try {
     mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    console.log('connecting to', config.MONGODB_URI)
 } catch (error) {
     console.log('mongoose connection failed')
 }
@@ -19,6 +20,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
+app.use('/api/user', userRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/sessions', sessionsRouter)
 
---
+++ controllers/user.js
@@ -0,0 +1,20 @@
+
+const userRouter = require('express').Router()
+const User = require('./../models/user')
+
+
+userRouter.get('/:id', async (request, response, next) => {
+    try {
+        const id = request.params.id
+
+        const user = await User
+            .findById(id)
+            .select({ "username": 1, "name": 1, "friends": 1, "sessions": 1})
+
+        response.json(user.toJSON())
+    } catch (error) {
+        next(error)
+    }
+})
+
+module.exports = userRouter
