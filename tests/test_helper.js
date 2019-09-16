77c5d1811afbfad363c39f48cb8bf034238c7ea6
Gustav Svensk <gustav.svensk95@gmail.com>
2019-09-16 17:12:06 +0200

    add consts to test helper

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -11,7 +11,67 @@ const usersInDb = async () => {
     return users.map(u => u.toJSON())
 }
 
+const initialSessions = [
+    {
+        dateTime: new Date('2019-09-20'),
+        location: "malmö",
+        creator: "5d7fa380cfcc10260c5cfc5b",
+        participants: [],
+        info: ""
+    },
+    {
+        dateTime: new Date('2020-01-01'),
+        location: "hangaren",
+        creator: "5d7fa380cfcc10260c5cfc5b",
+        participants: [],
+        info: ""
+    },
+    {
+        dateTime: new Date('2019-08-10'),
+        location: "solna",
+        creator: "5d7fa380cfcc10260c5cfc5b",
+        participants: [],
+        info: ""
+    },
+    {
+        dateTime: new Date('2019-10-01'),
+        location: "akalla",
+        creator: "5d7fa380cfcc10260c5cfc5b",
+        participants: [],
+        info: ""
+    }
+]
+
+const sortedSessions = [
+    {
+        dateTime: new Date('2020-01-01'),
+        location: "hangaren",
+        participants: [],
+        info: ""
+    },
+    {
+        dateTime: new Date('2019-10-01'),
+        location: "akalla",
+        participants: [],
+        info: ""
+    },
+    {
+        dateTime: new Date('2019-09-20'),
+        location: "malmö",
+        participants: [],
+        info: ""
+    },
+    {
+        dateTime: new Date('2019-08-10'),
+        location: "solna",
+        participants: [],
+        info: ""
+    }
+]
+
 module.exports = {
     usersInDb,
-    sessionsInDb
+    sessionsInDb,
+    initialSessions,
+    sortedSessions
 }
