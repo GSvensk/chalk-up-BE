c37d06455ecf4a0ed42840474d87130677f2aabd
Gustav Svensk <gustav.svensk95@gmail.com>
2019-09-16 12:05:34 +0200

    add bcrypt password hashing

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,13 @@
 const usersRouter = require('express').Router()
 const User = require('./../models/user')
+const bcrypt = require('bcrypt')
+
 
 usersRouter.get('/', async (request, response, next) => {
     try {
         const users = await User
             .find({})
-            .select({ "username": 1, "name": 1, "friends": 1, "sessions": 1})
+            .select({ "username": 1, "name": 1, "friends": 1, "sessions": 1 })
 
         response.json(users.map(user => user.toJSON()))
     } catch (error) {
@@ -15,13 +17,16 @@ usersRouter.get('/', async (request, response, next) => {
 
 usersRouter.post('/', async (request, response, next) => {
 
-    const body = request.body
-
     try {
+        const body = request.body
+
+        const saltRounds = 10
+        const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
         const user = new User({
-            "username": body.username,
-            "name": body.name,
-            "passwordHash": body.passwordHash,
+            username: body.username,
+            name: body.name,
+            passwordHash,
         })
 
         const savedUser = await user.save()
