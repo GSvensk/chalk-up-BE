554f054f117587d49442a2fc30db54f412cd7af6
Gustav Svensk <gustav.svensk95@gmail.com>
2019-09-16 12:20:23 +0200

    hide test password and username

--- utils/config.js
+++ utils/config.js
@@ -3,11 +3,16 @@ require('dotenv').config()
 let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
 
+let TEST_USERNAME = process.env.TEST_USERNAME
+let TEST_PASSWORD = process.env.TEST_PASSWORD
+
 if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI
 }
 
 module.exports = {
   PORT,
-  MONGODB_URI
+  MONGODB_URI,
+  TEST_USERNAME,
+  TEST_PASSWORD
 }
