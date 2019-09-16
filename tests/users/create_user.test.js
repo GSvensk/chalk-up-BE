58b6ad8147cd9e4dc9459c337e4f18a6d6e4170f
Gustav Svensk <gustav.svensk95@gmail.com>
2019-09-16 11:17:57 +0200

    add tests for user creation

--- tests/users/create_user.test.js
+++ tests/users/create_user.test.js
@@ -1,19 +1,97 @@
-const User = require('../models/user')
-const app = require('../app')
+const User = require('../../models/user')
+const helper = require('../test_helper')
+const supertest = require('supertest')
+const app = require('../../app')
+const api = supertest(app)
+const mongoose = require('mongoose')
 
+test('a user can be created', async () => {
+    await User.deleteMany({})
+    const usersAtStart = await helper.usersInDb()
 
-describe('when there is initially one user at db', () => {
+    const newUser = {
+        "username": "testeren",
+        "name": "testvard",
+        "passwordHash": "sekret"
+    }
+
+    await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd.length).toBe(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+})
+
+describe('when there is initially one user created', () => {
+
+    let newUserId = ""
+    let newUser = null
 
     beforeEach(async () => {
         await User.deleteMany({})
-        const user = new User(
-            {
-                "username": "testeren",
-                "name": "testvard",
-                "passwordHash": "sekret"
-            }
-        )
-        await user.save()
+        newUser = {
+            username: "testeren",
+            name: "testvard",
+            passwordHash: "sekret"
+        }
+
+        const res = await api
+            .post('/api/users')
+            .send(newUser)
+
+        newUserId = res.body.id
+    })
+
+    test('that user gets returned as json', async () => {
+        await api
+            .get(`/api/user/${newUserId}`)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+
+    test('the user is returned correctly', async () => {
+        const expectedUser =
+        {
+            id: newUserId,
+            username: "testeren",
+            name: "testvard",   
+            friends: [],
+            sessions: []
+        }
+        const res = await api.get(`/api/user/${newUserId}`)
+        expect(res.body).toMatchObject(expectedUser)
+    })
+
+
+    test('creation fails with proper statuscode and message if username already taken', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'testeren',
+            name: 'Superuser',
+            password: 'salainen',
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('`username` to be unique')
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd.length).toBe(usersAtStart.length)
     })
 })
 
+
+afterAll(() => {
+    mongoose.connection.close()
+})
